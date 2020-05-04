58c601b0fb1ca84ba6bf88a453f8300548468b9c
Arley Novais <nascimentoarle@gmail.com>
2020-05-04 19:43:20 -0400

    Version 0.1

--- app/src/main/java/com/br/arley/projeto2020/ui/PerfilActivity.java
+++ app/src/main/java/com/br/arley/projeto2020/ui/PerfilActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
